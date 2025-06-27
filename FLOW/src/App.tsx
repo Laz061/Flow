958e2add4f5f214e0e867a88ca8f9e46d289aed0
Laz061 <yhua536@aucklanduni.ac.nz>
2025-06-27 23:12:33 +1200

    Added the base of the 3D background and some styling of the todo list

--- FLOW/src/App.tsx
+++ FLOW/src/App.tsx
@@ -1,15 +1,23 @@
 import styles from "./styles/layout.module.css";
-import TodoList from "./Components/TodoList/TodoList";
 import "bootstrap-icons/font/bootstrap-icons.css";
+
+import TodoList from "./Components/TodoList/TodoList";
+import Vinyl from "./Components/Background/Vinyl";
 
 function App() {
   return (
     <div className={styles.appContainer}>
-      <div className={styles.content}>
-        <div className={styles.leftHalf}>
-          <TodoList></TodoList>
+      <div className={styles.background}>
+        <Vinyl />
+      </div>
+
+      <div className={styles.contentWrapper}>
+        <div className={styles.content}>
+          <div className={styles.leftHalf}>
+            <TodoList></TodoList>
+          </div>
+          <div className={styles.rightHalf}></div>
         </div>
-        <div className={styles.rightHalf}></div>
       </div>
     </div>
   );
---
+++ FLOW/src/Components/Background/Vinyl.tsx
@@ -0,0 +1,7 @@
+import Spline from "@splinetool/react-spline";
+
+export default function App() {
+  return (
+    <Spline scene="https://prod.spline.design/lJ8FRpJHuMmW1DKM/scene.splinecode" />
+  );
+}
